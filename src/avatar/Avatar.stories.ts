0d5d0ffe51cd4d84f41342ffcfbac05b8d895743
XyanZhang <1743067175@qq.com>
2023-05-21 11:06:28 +0800

    feat: story add avatar demo

---
+++ src/avatar/Avatar.stories.ts
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import reactLogo from '../assets/react.svg'
+import Avatar from './Avatar';
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
+const meta = {
+  title: 'Example/Avatar',
+  component: Avatar,
+  tags: ['autodocs'],
+} satisfies Meta<typeof Avatar>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {
+    src: reactLogo,
+    alt: 'React Logo',
+    title: 'React Logo Title',
+  },
+};
--- src/avatar/Avatar.tsx
+++ src/avatar/Avatar.tsx
@@ -1,12 +1,13 @@
 export interface AvatarProps {
   src: string;
   alt: string;
+  title?: string;
 }
 
 export default function Avatar(props: AvatarProps): JSX.Element {
   return (
     <div className="avatar">
-      <img src={props.src} alt={props.alt} />
+      <img src={props.src} alt={props.alt} title={props.title} />
     </div>
   );
 };

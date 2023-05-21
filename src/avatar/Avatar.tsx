6ab3aea7da102f9a7d3bdbb0138410931193af6c
XyanZhang <1743067175@qq.com>
2023-05-21 12:23:06 +0800

    feat: Avatar

--- src/avatar/Avatar.tsx
+++ src/avatar/Avatar.tsx
@@ -1,13 +1,95 @@
-export interface AvatarProps {
-  src: string;
-  alt: string;
-  title?: string;
+import React, { ReactNode, CSSProperties, useRef, useState } from 'react';
+import classNames from 'classnames';
+
+import './index.scss';
+
+export interface avatarProps extends React.HTMLAttributes<HTMLDivElement> {
+  /**
+   * 按钮大小
+   */
+  size?: number | 'small' | 'medium' | 'large';
+  shape?: 'circle' | 'square';
+  src?: string | ReactNode;
+  className?: string;
+  icon?: React.ReactNode;
+  gap?: number;
+  children?: ReactNode;
+  style?: CSSProperties;
+  alt?: string;
 }
 
-export default function Avatar(props: AvatarProps): JSX.Element {
+const Avatar = (props: avatarProps) => {
+  const {
+    size = 'medium',
+    shape = 'circle',
+    src,
+    icon,
+    gap,
+    children,
+    ...others
+  } = props;
+
+  const [scale, setScale] = useState(1);
+  const wraperRef = useRef<HTMLElement>(null)
+  const textRef = useRef(null)
+
+  const textRefCallback = React.useCallback( (node: HTMLElement) => {
+    if(!node) return;
+    const reRender = () => {
+      const wraperNode = wraperRef.current;
+      if (!node || !wraperNode) {
+        return;
+      }
+      const wraperWidth = wraperNode.offsetWidth;
+      const textWidth = node.offsetWidth;
+      const gap = 4;
+
+      const scale = wraperWidth - gap * 2 < textWidth ?
+        (wraperWidth - gap * 2) / textWidth : 1;
+      setScale(scale);
+    }
+
+    const ob = new ResizeObserver(reRender);
+    ob.observe(node);
+
+  }, [])
+
+  const cls = classNames({
+    'ant-avatar': true,
+    'ant-avatar-lg': size === 'large',
+    'ant-avatar-sm': size === 'small',
+    'ant-avatar-icon': icon,
+    'ant-avatar-image': src,
+    [`ant-avatar-${shape}`]: shape,
+  });
+
+  const style = typeof size === 'number' ? {
+    width: size,
+    height: size,
+    lineHeight: `${size}px`,
+    fontSize: size / 2,
+  } : props.style;
+
+  const textStyle = {
+    lineHeight: `${size}px`,
+    transform: `scale(${scale}) translateX(-50%)`
+  }
+
   return (
-    <div className="avatar">
-      <img src={props.src} alt={props.alt} title={props.title} />
-    </div>
+    <span
+      className={cls}
+      {...others}
+      style={style}
+      ref={wraperRef}
+    >
+      {icon ? icon : null}
+      {src ? (typeof src === 'string' ? <img src={src} /> : src) : null}
+      {children ? (typeof children === 'string' ? <span
+        style={textStyle}
+        ref={textRefCallback}
+        className="ant-avatar-string">{children}</span> : children) : null}
+    </span>
   );
-};
+}
+
+export default Avatar;

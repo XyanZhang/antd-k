f7f5f4c71f8afdc4cac785161a923850930c7d4c
XyanZhang <1743067175@qq.com>
2023-05-22 15:25:49 +0800

    feat: add icon avatar demo

--- src/avatar/Avatar.stories.tsx
+++ src/avatar/Avatar.stories.tsx
@@ -2,6 +2,7 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import Avatar from './index';
 import { circleImageSrc } from '../utils';
+import StarTwoTone from '@ant-design/icons/StarTwoTone';
 
 const meta = {
   title: 'DataDisplay/Avatar',
@@ -13,7 +14,7 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Uses: Story = { // 配置了这些参数的作用：1. 生成文档 2. 生成控制面板
+export const ImageSrc: Story = { // 配置了这些参数的作用：1. 生成文档 2. 生成控制面板
   args: {
     src: circleImageSrc(),
     shape: 'circle', // 'square' | 'circle'
@@ -21,6 +22,22 @@ export const Uses: Story = { // 配置了这些参数的作用：1. 生成文档
     gap: 4,
     className: '1233',
     icon: '',
+    children: 'avatar',
+    style: {
+      backgroundColor: '#ccc',
+    },
+    title: 'avatar title',
+    alt: "avatar alt"
+  },
+};
+export const Icon: Story = { // 配置了这些参数的作用：1. 生成文档 2. 生成控制面板
+  args: {
+    src: circleImageSrc(),
+    shape: 'circle', // 'square' | 'circle'
+    size: 64,
+    gap: 4,
+    className: '1233',
+    icon: <StarTwoTone />,
     children: 'avatar',
     style: {
       backgroundColor: '#ccc',

b1b1043771c06fb4096518c3c2ba59c6004b56a6
XyanZhang <1743067175@qq.com>
2023-05-22 18:30:07 +0800

    feat: button type shape

---
+++ src/button/Button.stories.tsx
@@ -0,0 +1,44 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import Button, { ButtonTypes } from './Button';
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
+const meta = {
+  title: 'General/Button',
+  component: Button,
+  tags: ['autodocs'],
+  argTypes: {
+    type: {
+      options: ButtonTypes,
+      control: {
+        type: "select",
+      }
+    },
+    shape: {
+      options: ['default', 'circle', 'round'],
+      control: {
+        type: "radio",
+      },
+    },
+    size: {
+      options: ['sm', 'md', 'lg'],
+      control: {
+        type: "radio",
+      },
+    }
+  }
+} satisfies Meta<typeof Button>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const Primary: Story = {
+  args: {
+    type: 'primary',
+    children: "button",
+    shape: "default",
+    danger: true,
+    size: 'md'
+  },
+};
--- src/button/Button.tsx
+++ src/button/Button.tsx
@@ -1,48 +1,42 @@
-import React from 'react';
-import './button.css';
+import React, { ReactNode } from 'react';
+import classNames from 'classnames';
 
-interface ButtonProps {
-  /**
-   * Is this the principal call to action on the page?
-   */
-  primary?: boolean;
-  /**
-   * What background color to use
-   */
-  backgroundColor?: string;
-  /**
-   * How large should the button be?
-   */
-  size?: 'small' | 'medium' | 'large';
-  /**
-   * Button contents
-   */
-  label: string;
-  /**
-   * Optional click handler
-   */
-  onClick?: () => void;
+import './index.scss';
+
+export type ButtonType = 'normal' | 'primary' | 'dashed' | 'link' | 'text';
+interface buttonProps extends React.HTMLAttributes<HTMLButtonElement> {
+  className?: string;
+  type?: ButtonType;
+  size?: 'sm' | 'lg' | 'md';
+  shape?: 'default' | 'circle' | 'round';
+  children?: ReactNode | string;
+  style?: React.CSSProperties;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
+  onBlur?: React.FocusEventHandler<HTMLButtonElement>;
+  htmlType?: "button" | "submit" | "reset";
+  danger?: boolean;
 }
 
-/**
- * Primary UI component for user interaction
- */
-export const Button = ({
-  primary = false,
-  size = 'medium',
-  backgroundColor,
-  label,
-  ...props
-}: ButtonProps) => {
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
-  return (
-    <button
-      type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
-      style={{ backgroundColor }}
-      {...props}
-    >
-      {label}
-    </button>
-  );
-};
+export const ButtonTypes = ['normal', 'primary', 'dashed', 'link', 'text'];
+
+const Button = React.forwardRef<HTMLButtonElement, buttonProps>((props: buttonProps, ref) => {
+  const { className, type = 'normal', size = 'md', children, style, onClick, onBlur,
+    htmlType = 'button',
+    shape = 'default',
+    danger = false,
+    ...others } = props;
+
+  const cls = classNames({
+    'ant-btn': true,
+    [`ant-btn-${size}`]: size,
+    [`ant-btn-${type}`]: type,
+    [`ant-btn-${shape}`]: shape,
+    [`ant-btn-dangerous`]: danger,
+    [className as string]: !!className,
+  })
+  return <button {...others}
+    type={htmlType}
+    ref={ref} className={cls} style={style} onClick={onClick} onBlur={onBlur}>{children}</button>;
+})
+
+export default Button;
